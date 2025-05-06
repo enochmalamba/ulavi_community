43c216a9a80447aabf484d82ea47fb7977189371
enochmalamba <enochmalamba@gmail.com>
2025-05-06 16:17:10 +0200

    created index.php for the landing page. am out!

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+// echo 'hello world';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="includes/index.css">
+    <title>ULAVi Community</title>
+</head>
+
+<body>
+    <header>
+        <h1>Welcome to the ULAVI Community</h1>
+    </header>
+    <div class="hero">
+        <div class="hero-container">
+            <div class="hero-text">
+                <img src="logo.png" alt="localsketch logo" class="logo">
+                <h2 class="hero-title">
+                    Share. Connect. Collaborate.
+                </h2>
+                <p class="hero-text">
+                    Join the vibrant ULAVI Community – a space for artists, enthusiasts, and collaborators to connect,
+                    grow,
+                    and shape the future of Malawian arts.
+                </p>
+                <div class="hero-btns">
+                    <a href="https://ulavi.org/community" class="sign-up">Sign up</a>
+                    <a href="https://ulavi.org/community" class="sign-in">Sign in</a>
+                    <div></div>
+                </div>
+
+            </div>
+            <div class="hero-media">
+                <iframe width="100%" height="600" src="https://www.youtube.com/embed/OTfJDFt2TLY"
+                    title="Coast Contra: Evolved Nostalgia | Flow State with Harry Mack #10" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </div>
+        </div>
+
+    </div>
+</body>
+
+</html>
